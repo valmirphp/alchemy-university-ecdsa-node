445267add2769866d63abca2689c3ab72757b5b7
Valmir Barbosa <valmir.php@gmail.com>
2023-08-22 00:47:14 -0300

    feat: conf env VITE_API_URL

--- client/src/services/server.ts
+++ client/src/services/server.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {ApiService} from "./api.service.ts";
 
 export const server = axios.create({
-  baseURL: 'http://localhost:3042',
+  baseURL: process.env.VITE_API_URL || 'http://localhost:3000',
 });
 
 export const api = new ApiService(server);
